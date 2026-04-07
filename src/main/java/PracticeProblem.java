305e924b2085acfc6a21b7e5a114e4ab4a5876ab
unikornnaphat <350364092@tdsb.ca>
2026-04-07 01:21:11 +0000

    3.4-sandy

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,29 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+    public static int findSmallest(int a, int b, int c) {
+		int temp = Math.min(a, b);
+        return Math.min(temp, c);
+    }
 
+    public static boolean isLeapYear(int year) {
+
+		if (year % 4 == 0) {
+
+			if (year % 100 == 0) {
+
+				if (year % 400 == 0) {
+					return true;
+                } else {
+
+					return false;
+                }
+            } else {
+				return true;
+            }
+        } else {
+			
+			return false;
+        }
+    }
 }
